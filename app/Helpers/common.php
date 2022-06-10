f383bd33680da454727c5b43d1e7d55f19d750a5
Đỗ Cao Tự <tu.doitdn@gmail.com>
2022-06-10 11:05:12 +0700

    Export

---
+++ app/Helpers/common.php
@@ -0,0 +1,31 @@
+<?php
+
+if (! function_exists('dump_data')) {
+    function dump_data() {
+        $args = func_get_args();
+        echo '<pre>';
+        foreach($args as $ag) {
+            print_r($ag);
+        }
+        echo '<pre>';
+        die();
+    }
+}
+
+if (! function_exists('convertArrayToString')) {
+    function convertArrayToString($dataArr, $keyValue) {
+        $valueArr = [];
+        foreach ($dataArr as $key => $obj) {
+            $valueArr[] = $obj[$keyValue];
+        }
+        return implode(', ', $valueArr);
+    }
+}
+
+if (! function_exists('isValidTimeStamp')) {
+    function isValidTimeStamp($string) {
+        return ( 1 === preg_match( '~^[1-9][0-9]*$~', $string ) );
+    }
+}
+
+
