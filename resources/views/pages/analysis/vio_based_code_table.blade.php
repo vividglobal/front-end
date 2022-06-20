8e8d958d22d8cfb95e5b4ddf119d1e1a27831e1c
Nguyễn Hoàng Huy <huynguyen@hekategroup.com>
2022-06-20 17:08:21 +0700

    ui

--- resources/views/pages/analysis/vio_based_code_table.blade.php
+++ resources/views/pages/analysis/vio_based_code_table.blade.php
@@ -1,4 +1,5 @@
 <table class="table_analysis">
+<<<<<<< HEAD
     <thead>
         <tr>
             <th>No</th>
@@ -39,6 +40,32 @@
         </tr>
         @endforeach
     </tbody>
+=======
+    <tr>
+        <th>No</th>
+        <th style="width: 19.2%;">
+        {{ __('Code article') }}
+        </th>
+        <th style="width: 53.5%;">
+        {{ __('Violation type') }}
+        </th>
+        <th style="width: 14.8%;">
+        <div class="th-title-right_analysis flex-space_beetween">
+            <p>{{ __('Articles') }}</p>
+            <img class="ico-sort theard-table sort_up" data-date="datePost" src="../assets/image/Archive/up_enable.svg" alt="">
+            <img class="ico-sort theard-table sort_down" data-date="datePost" src="../assets/image/Archive/down_enable.svg" alt="">
+        </div>
+        </th>
+    </tr>
+    @foreach($codeData as $key => $code)
+    <tr>
+        <td>{{ $key + 1 }}</td>
+        <td>{{ $code->name }}</td>
+        <td>{{ $code->type_name }}</td>
+        <td>{{ $code->total_article }}</td>
+    </tr>
+    @endforeach
+>>>>>>> 8fe4ebc (ui)
 </table>
 <div class="row-pagination">
     {{ $codeData->links('layouts.my-paginate') }}
