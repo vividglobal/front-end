d6c9cb7290de86022776e34a6600012cebbca38d
Nguyễn Hoàng Huy <huynguyen@hekategroup.com>
2022-07-07 17:34:14 +0700

    modal.js

--- public/assets/js/autodetect/modal.js
+++ public/assets/js/autodetect/modal.js
@@ -19,7 +19,9 @@ $(document).ready(function () {
         imageModal.find('img').attr('src', imgSrc)
         imageModal.show();
         $(".mdl-js").css("overflow-y","hidden");
+
     });
+
     btn.click(function () {
         let caption = $(this).find('a').text();
         let brandName = $(this).parents('tr').find('.brand-name').text();
@@ -36,8 +38,29 @@ $(document).ready(function () {
         $('.div-item').remove();
         $('.no-file-remove').remove();
         $(".mdl-js").css("overflow-y","scroll");
-        $("div#box_list_file").removeClass("row-upload")
+
     });
+    $(window).on('click', function (e) {
+        if ($(e.target).is('.modal-title')) {
+            captionModal.hide();
+            $(".mdl-js").css("overflow-y","scroll");
+
+        }
+        if ($(e.target).is('.modal-upload-file')) {
+            uploadModal.hide();
+            btnuploadfile.hide();
+            $('.div-item').remove();
+            $('.no-file-remove').remove();
+            $(".mdl-js").css("overflow-y","scroll");
+
+        }
+        if ($(e.target).is('.modalimg')) {
+            imageModal.hide();
+        $(".mdl-js").css("overflow-y","scroll");
+        }
+        // $(".mdl-js").css("overflow-y","scroll");
+    });
+
     let rowId =""
     uploadfile.click(function(e) {
         $(this).addClass("check")
@@ -51,23 +74,17 @@ $(document).ready(function () {
     })
 
     $(document).on('change', '.file-input', async function(){
+        $('.no-file-remove').remove();
         let flag = true
         let form = new FormData();
         let files = $('#upload')[0].files;
         if (files.length > 5) {
             flag = false
-            show_error("You are only allowed to upload a maximum of 5 files at a time")
+            alert('You are only allowed to upload a maximum of 5 files at a time');
         }
         if(files.length !== 0 && flag){
             show_overlay()
             for(let i = 0; i < files.length; i++){
-                let extension = (files[i].name).split('.').pop().toLowerCase();
-                if ($.inArray(extension, ['pdf']) == -1) {
-                    hide_overlay()
-                    show_error("You have uploaded files that are not in the correct PDF format")
-                    return false;
-                }
-                $('.no-file-remove').remove();
             form.append("document", files[i]);
             form.append("article_id", rowId);
                 let settings = {
@@ -115,10 +132,6 @@ $(document).ready(function () {
                     }
                 }
                 show_success(message);
-                let chekclengthfile = $("div#box_list_file").children().length
-                if(chekclengthfile >= 12){
-                    $("div#box_list_file").addClass("row-upload")
-                }
             }
             hide_overlay()
             $('#upload').val('');
@@ -134,9 +147,6 @@ $(document).ready(function () {
                 if(res){
                     loading.hide()
                     btnuploadfile.show()
-                    if((res.data).length >= 12){
-                        $("div#box_list_file").addClass("row-upload")
-                    }
                     if((res.data).length>0){
                         for(let i = 0 ; i<(res.data).length;i++){
                             fileHtmlItems = `<div class="col-sm-3 col-md-3 col-lg-3 mb-2 items_file div-item">
@@ -175,39 +185,48 @@ $(document).ready(function () {
     $(document).on("click", '.delete-file', async function (){
         let rowIdelemnet = $(this).attr("id-delete");
         let parentItem = $(this).parents('.items_file')
+        let filesNumber = $(".item-one-file").length
+        DeleteFile(rowIdelemnet,parentItem,filesNumber)
+    })
+
+    function DeleteFile(rowIdelemnet,parentItem,filesNumber){
         show_overlay()
-        let response = await $.ajax({
+        $.ajax({
             headers: {
                 'X-CSRF-TOKEN': csrf,
             },
+            data :{
+                "article_id": filesNumber <= 1 ? rowId : ""
+            },
             method: "DELETE",
             url:"/articles-document/"+rowIdelemnet+"",
-        });
-        let chekclengthfile = $("div#box_list_file").children().length
-        if(chekclengthfile <= 13){
-            $("div#box_list_file").removeClass("row-upload")
-        }
-        hide_overlay()
-        if(response.success) {
-            parentItem.remove();
-            let item = $('.div-item')
-            if(item.length === 0){
-                $(".entry").find(`#${rowId}`).attr("src","../assets/image/lega1.png")
-                $(".date-penalty").find(`#${rowId}`).text("")
-            }
-<<<<<<< HEAD
-            show_success("Old document deleted successfully!");
-=======
-            show_success(response.message);
-            if(!$(".item-one-file").is(":visible")){
-                let progressStatus =  $(`#${rowId}`).find(".track:nth-child(8)").find(".entry").find(".list--status")
-                .find(".select--status").find(".select__one--status:nth-child(3)")
-                if(progressStatus.hasClass("show")){
-                    progressStatus.addClass("hide").removeClass("show")
+        })
+        .done(function( msg){
+            if(msg){
+                hide_overlay()
+                parentItem.remove();
+                show_success(msg.message);
+                let item = $('.div-item')
+                if(item.length === 0){
+                    $(".entry").find(`#${rowId}`).attr("src","../assets/image/lega1.png")
+                    $(".date-penalty").find(`#${rowId}`).text("")
+                    let progressStatus =  $(`#${rowId}`).find(".track:nth-child(8)").find(".entry").find(".list--status")
+                    .find(".select--status")
+                    if(progressStatus.find(".select__one--status:nth-child(3)").hasClass("show")){
+                        progressStatus.find(".select__one--status:nth-child(3)").addClass("hide").removeClass("show")
+                    }
+                    let getTextStatus = $(`#${rowId}`).find(".track:nth-child(8)").find(".entry").find(".list--status").find("> p")
+                    if(getTextStatus.text().trim() == "Completed"){
+                        progressStatus.find(".select__one--status:first-child").addClass("background-gray")
+                        progressStatus.find(".select__one--status:first-child").find("img").show()
+                        getTextStatus.text("Not started").attr("data-id","not_started")
+                    }
                 }
             }
->>>>>>> a49decc (fix Ui)
-        }else {
-        }
-    })
+        })
+        .fail(function(error){
+            show_error(error.responseJSON.message);
+            hide_overlay()
+        })
+    }
 });
