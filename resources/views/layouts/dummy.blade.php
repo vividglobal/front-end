44a0836cf8e6cf6f64e542c5b078ae47bc891d10
Nguyễn Hoàng Huy <huynguyen@hekategroup.com>
2022-08-19 11:36:08 +0700

    fix favicon and title login

--- resources/views/layouts/dummy.blade.php
+++ resources/views/layouts/dummy.blade.php
@@ -6,7 +6,8 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title')</title>
+    <title>VIVID Global</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/image/favicon.png') }}">
     <meta name="Description" content="@yield('description')">
 
     <!-- Fonts -->
